7216b27a736ba736b1126f5326d24b779fccc2da
saminoorsyed <saminoorsyed@gmail.com>
2023-09-19 10:14:29 -0400

    add in basic styling and Teutsch logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import logo from "./logo.svg";
+import logo from "./images/teutsch.png";
 import "./App.css";
 
-function App() {
+
+
+function App(): JSX.Element {
   return (
     <div className="main">
       <section className="calculator-body">
-        <input type="text" value={"TODO: make this work ;)"} id="calculator-input" />
-        <img src={logo} className="react-logo" alt="logo" />
+        <div className="input-logo">
+          <div className="top-bar">
+            <input type="text" value={"TODO: make this work ;)"} id="calculator-input" />
+            <button className="del-button">del</button>
+          </div>
+          <img src={logo} className="teutsch-logo" alt="logo" />
+        </div>
+        <div>
+          <button className="calculator-button">+</button>
+          <button className="calculator-button">-</button>
+          <button className="calculator-button">*</button>
+          <button className="calculator-button">^</button>
+        </div>
         <div>
           <button className="calculator-button">7</button>
           <button className="calculator-button">8</button>
---
+++ src/images/components/CalcButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CalcButton(): JSX.Element {
+  return (
+    <button className="calculator-button">7</button>
+  )
+}
+
+export default CalcButton
