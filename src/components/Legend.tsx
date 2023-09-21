ee60d9473d309c5c7e629c77c00f4bf12b344315
saminoorsyed <saminoorsyed@gmail.com>
2023-09-21 18:09:25 -0400

    adjust keyboard directions to look nicer

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -2,12 +2,20 @@ import React from 'react'
 
 function Legend() {
     return (
+        <>
+        <p> <strong>Keyboard Directions:</strong></p>
         <div className="directions-container" >
-            <p className="directions"> <strong>Keyboard Directions:</strong></p>
-            <p className="directions">"*"= multilpy, "/" = divide, "+" = add, "-"== subtract, "^"= raise the the power of,</p>
-            <p className="directions">"=" or "Enter" computes the current sum,"Esc" clears all input, </p>
-            <p className="directions">"Backspace" removes the last character input, "n" multiplies the current number by -1</p>
+            <p className="directions">"*"= multiply </p>
+            <p className="directions">"/" = divide</p>
+            <p className="directions">"+" = add</p>
+            <p className="directions">"-"=subtract </p>
+            <p className="directions"> "^"= exponent</p>
+            <p className="directions">"=" or "Enter"</p>
+            <p className="directions">"Esc" clears all input</p>
+            <p className="directions">"Backspace" removes the last char</p>
+            <p className="directions">"n" multiply current number by -1</p>
         </div>
+        </>
     )
 }
 
